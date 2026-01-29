1274b5a2ee6768fba71a8cec678535d0fbadcc0a
ar1151 <ar1151@exeter.ac.uk>
2026-01-29 10:03:57 +0000

    HelloWorldApp created

---
+++ HelloWorldApp.java
@@ -0,0 +1,7 @@
+public class HelloWorldApp{
+    public static void main(String[] args){
+        System.out.print("Hello World! My name is " + args[0] + ".");
+        System.out.println(" I am " + args[1] + " years old.");
+    }
+}
+
